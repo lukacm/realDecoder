9abafc453b5a5250bcae5edb5d654c56c3ef48c0
martin_lukac <lukacm@daklands.mine.nu>
2019-09-17 22:43:52 +0600

    lukacm

--- readRealPython.py
+++ readRealPython.py
@@ -4,6 +4,21 @@ import sys
 import numpy as np
 import re
 
+def swap(i, wires):
+    current = ID
+    print(i)
+    for j in range(0, wires-2):
+        if j == i:
+            current = np.kron(current, SWAP)
+        else:
+            current = np.kron(current,ID)
+    if i > j:
+            current = np.kron(current, SWAP)
+    return current
+
+
+
+
 #Default Matrices
 ID = np.array([[1,0],[0,1]])
 SWAP = np.array([[1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]])
@@ -25,13 +40,15 @@ for i in content:
         numvars = int(result.group(1))
     if i.startswith(".variables"):
         result = exp2.search(i)
-        variables = np.flip(result.group(1).split(), axis=0)
+        variables = result.group(1).split()
+        variables.reverse()
         print(variables)
     gates =+1
 
 
 # Create all Toffoli gates
 matrices = dict()
+circuit = np.empty([gates,numvars,numvars])
 for i in range(numvars):
     a = np.eye(pow(2,(i+1)))
     #Reverse the bottom NOT
@@ -44,17 +61,48 @@ for i in range(numvars):
 
 #Process the real array
 flag = 0
+exp3 = re.compile(".(\d+)")
 for i in content:
     if i.startswith(".end"):
         flag = 0
     if flag:
         vars = i.split()
         current = matrices[vars[0]]
-        for j in range(numvars-int(vars[0][1])):
-            print('multiple: %i' %j)
+        result = exp3.search(vars[0])
+        res = result.group(1).split()
+        size = int(res[0])
+        # create the k-controlled toffoli aligned with bottom bit
+        for j in range(numvars-size):
+            #print('multiple: %i' %j)
             next = np.kron(ID,current)
-            print(next)
-            #current = next
+            print(next.shape)
+            current = next
+        nominal = variables
+        target = vars[1:]
+        print(len(target))
+        diff = numvars - len(target)
+        print(target)
+        print(nominal)
+        front = np.eye(pow(2,numvars))
+        print(front.shape)
+        back = np.eye(pow(2,numvars))
+        for t in range(len(target)-1,-1,-1):
+            nominal = variables.copy()
+            #print(target[t])
+            if target[t] != nominal[t+diff]:
+                print('orig: {} current {}'.format(nominal[t+diff], target[t]))
+                indx = nominal.index(target[t])
+                swpsnum = abs(t+diff-indx)
+                print('Needs {}'.format(swpsnum))
+                for q in range(0,swpsnum):
+                    print(indx)
+                    sw = swap(indx, numvars)
+                    front = front*sw
+                    back = sw*back
+                    nominal[indx+q],nominal[indx+q+1] = nominal[indx+q+1],nominal[indx+q]
+                print(nominal)
+        current = back+current*front
     if i.startswith(".begin"):
         flag = 1
+
 
